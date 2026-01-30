ec75d1360e891293d875298642865f0b796def8a
joelvsam <151171325+joelvsam@users.noreply.github.com>
2026-01-30 11:55:14 +0400

    llm changed

--- backend/ai/llm.py
+++ backend/ai/llm.py
@@ -10,7 +10,7 @@ from backend.core.config import HUGGINGFACEHUB_API_TOKEN
 # Create a HuggingFaceEndpoint instance to interact with the language model.
 # The HuggingFaceEndpoint class allows us to use a model from the Hugging Face Hub as a language model.
 llm = HuggingFaceEndpoint(
-    repo_id="stanford-crfm/BioMedLM",
+    repo_id="google/flan-t5-large",
     task="text2text-generation",
     huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
     temperature=0.5,
